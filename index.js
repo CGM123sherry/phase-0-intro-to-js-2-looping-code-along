bbf15e39438e9c7932422175604b8600987a96ee
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-03 22:41:13 +0300

    completed the looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+  //an empty array
+  let messages = [];
+  //loop
+  for (let i = 0; i < names.length; i++) {
+    //appreciation message
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
